2df38bd92a692b3db3b1657e0f70bd2e25c81909
brian-ambani <brainambani1@gmail.com>
2023-11-29 13:14:43 +0300

    cleaned up files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
-      <h1>Hello react typescript</h1>
+      <Button onClick={addCount}>Clicked {count} times</Button>
     </div>
   );
 }

5f50f8973903636c45f4309c884dd63d429cb1f4
brian-ambani <brainambani1@gmail.com>
2023-11-29 11:13:35 +0300

    updated the app file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,12 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import NoteModel from "./models/note";
+import notesRoutes from "./routes/notesRoute";
 
 const app = express();
 
-app.get("/", async (req, res, next) => {
-  try {
-    // throw new Error("Server error");
-    const notes = await NoteModel.find().exec();
-    res.status(200).json(notes);
-  } catch (error) {
-    next(error);
-  }
-});
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
   next(Error("Endpoint not found"));

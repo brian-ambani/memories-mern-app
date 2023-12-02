7e0047af512445b0294eb8a9a9a51626e099ab3c
brian-ambani <brainambani1@gmail.com>
2023-12-02 04:00:06 +0300

    updated error handling when the database fail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,31 @@ import stylesUtils from "./styles/utils.module.css";
 import { useState, useEffect } from "react";
 import { Note as NoteModel } from "./models/noteModel";
 import Note from "./components/Note";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Spinner } from "react-bootstrap";
 import * as notesApi from "./network/notes_api";
 import AddEditNoteDialog from "./components/AddEditNoteDialogue";
 import { FaPlus } from "react-icons/fa";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [notesLoading, setNotesLoading] = useState(true);
+  const [showNotesLoadingError, setShowNotesLoadingError] = useState(false);
+
   const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
   const [noteToEdit, setNoteToEdit] = useState<NoteModel | null>(null);
 
   useEffect(() => {
     async function getNotes() {
       try {
+        setShowNotesLoadingError(false);
+        setNotesLoading(true);
         const notes = await notesApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
-        alert(error);
+        setShowNotesLoadingError(true);
+      } finally {
+        setNotesLoading(false);
       }
     }
     getNotes();
@@ -35,8 +42,24 @@ function App() {
       alert(error);
     }
   }
+
+  const notesGrid = (
+    <Row xs={1} md={2} xl={3} className={`g-4 ${styles.noteGrid}`}>
+      {notes.map((note) => (
+        <Col key={note._id}>
+          <Note
+            note={note}
+            className={styles.note}
+            onNoteClicked={setNoteToEdit}
+            onDeleteNoteClicked={deleteNote}
+          />
+        </Col>
+      ))}
+    </Row>
+  );
+
   return (
-    <Container>
+    <Container className={styles.notePage}>
       <Button
         className={`mb-4 ${stylesUtils.blockCenter} ${stylesUtils.flexCenter}}`}
         onClick={() => setShowAddNoteDialog(true)}
@@ -44,18 +67,21 @@ function App() {
         <FaPlus className="me-2" />
         Add new note
       </Button>
-      <Row xs={1} md={2} xl={3} className="g-4">
-        {notes.map((note) => (
-          <Col key={note._id}>
-            <Note
-              note={note}
-              className={styles.note}
-              onNoteClicked={setNoteToEdit}
-              onDeleteNoteClicked={deleteNote}
-            />
-          </Col>
-        ))}
-      </Row>
+      {notesLoading && <Spinner animation="border" variant="primary" />}
+      {showNotesLoadingError && (
+        <div className={stylesUtils.blockCenter}>
+          <p className="text-danger">Error loading notes.</p>
+          <Button onClick={() => window.location.reload()}>Retry</Button>
+        </div>
+      )}
+      {!notesLoading && !showNotesLoadingError && (
+        <>
+          {notes.length === 0 && (
+            <p className={stylesUtils.blockCenter}>No notes yet.</p>
+          )}
+          {notes.length > 0 && notesGrid}
+        </>
+      )}
       {showAddNoteDialog && (
         <AddEditNoteDialog
           onDismiss={() => setShowAddNoteDialog(false)}
